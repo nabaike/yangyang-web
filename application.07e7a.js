9603520fef41f87ef4f859c551246461339ad32d
nabaike <nabaike@gmail.com>
2026-07-18 01:59:40 +0800

    anti-yang: 薄荷绿背景+退回/暂存/刷新功能键+循环刷新图标 20260718-015940

--- application.07e7a.js
+++ application.07e7a.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.c7b6c.json';
+          this.settingsPath = 'src/settings.a11c1.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- index.6eb50.js
+++ index.6eb50.js
@@ -1,4 +1,4 @@
-System.register(["./application.8511f.js"], function (_export, _context) {
+System.register(["./application.07e7a.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
