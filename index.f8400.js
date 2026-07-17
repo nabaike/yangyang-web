54d23802b46a35b27e765958dcceb0acf108e6fb
nabaike <nabaike@gmail.com>
2026-07-17 10:11:14 +0800

    update web 20260717-101114

--- index.f8400.js
+++ index.f8400.js
@@ -1,4 +1,4 @@
-System.register(["./application.5dbf3.js"], function (_export, _context) {
+System.register(["./application.a76e6.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
