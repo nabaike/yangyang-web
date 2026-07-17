8b8edb13bc8647c5d953d3b8b9b3aa1bf0e307a9
nabaike <nabaike@gmail.com>
2026-07-18 02:05:36 +0800

    labels: 木牌字改用快乐体(原字体) 020536

--- index.eabe4.js
+++ index.eabe4.js
@@ -1,4 +1,4 @@
-System.register(["./application.07e7a.js"], function (_export, _context) {
+System.register(["./application.ab858.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
