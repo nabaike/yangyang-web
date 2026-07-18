f0fea001754f8a7497fc9005e2572e89f2a509b1
nabaike <nabaike@gmail.com>
2026-07-18 16:52:37 +0800

    update web 20260718-165237

--- index.f93dc.js
+++ index.f93dc.js
@@ -1,4 +1,4 @@
-System.register(["./application.f30fb.js"], function (_export, _context) {
+System.register(["./application.bc87a.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
