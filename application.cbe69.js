ac0fda18721ab2b49117fa7338282a4e7f9f799e
nabaike <nabaike@gmail.com>
2026-07-18 22:43:20 +0800

    update web 20260718-224320

--- application.cbe69.js
+++ application.cbe69.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.aaba8.json';
+          this.settingsPath = 'src/settings.ecd45.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
