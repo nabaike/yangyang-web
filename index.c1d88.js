004cf81946b03d9d16a1cbde055899f974607615
nabaike <nabaike@gmail.com>
2026-07-20 01:53:36 +0800

    update web 20260720-015336

--- index.c1d88.js
+++ index.c1d88.js
@@ -1,4 +1,4 @@
-System.register(["./application.3fb8d.js"], function (_export, _context) {
+System.register(["./application.b5494.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
