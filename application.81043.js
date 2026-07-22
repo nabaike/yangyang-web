3cd358ecb46fdaaef7c9a21a1c4901cea90d2cd3
nabaike <nabaike@gmail.com>
2026-07-23 02:06:31 +0800

    update web 20260723-020631

--- application.81043.js
+++ application.81043.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.d2b4b.json';
+          this.settingsPath = 'src/settings.e131e.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
