c3c945d732f266ecf9803ae6402908ebedfbbcea
nabaike <nabaike@gmail.com>
2026-07-25 23:30:41 +0800

    update web 20260725-233041

--- index.7fa9d.js
+++ index.7fa9d.js
@@ -1,4 +1,4 @@
-System.register(["./application.d0e87.js"], function (_export, _context) {
+System.register(["./application.114cf.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
