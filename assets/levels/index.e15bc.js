7ec7182e23d30d2fc5abc27c6fd2048261d8f080
nabaike <nabaike@gmail.com>
2026-07-28 20:55:22 +0800

    update web 20260728-205522

---
+++ assets/levels/index.e15bc.js
@@ -0,0 +1,20 @@
+System.register("chunks:///_virtual/levels",[],(function(){return{execute:function(){}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports/levels', 'chunks:///_virtual/levels'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    return {
+        setters: [function(_m) {
+            var _exportObj = {};
+
+            for (var _key in _m) {
+              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+            }
+      
+            _export(_exportObj);
+        }],
+        execute: function () { }
+    };
+    });
+});
