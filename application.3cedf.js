1a8548a4a70ce9bdccc500b183b0f3196b4abd90
nabaike <nabaike@gmail.com>
2026-09-07 22:28:31 +0800

    update web 20260907-222831

--- application.3cedf.js
+++ application.3cedf.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.42137.json';
+          this.settingsPath = 'src/settings.44039.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
