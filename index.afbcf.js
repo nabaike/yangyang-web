4b61397af7878a0526ae868f61a197072fb26d07
nabaike <nabaike@gmail.com>
2026-09-09 22:59:30 +0800

    update web 20260909-225929

--- index.afbcf.js
+++ index.afbcf.js
@@ -1,4 +1,4 @@
-System.register(["./application.b6364.js"], function (_export, _context) {
+System.register(["./application.06031.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
