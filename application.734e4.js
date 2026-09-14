8f867544ca36195015ea6fdecfaf5aa3e87080ab
nabaike <nabaike@gmail.com>
2026-09-14 22:16:02 +0800

    update web 20260914-221602

--- application.734e4.js
+++ application.734e4.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.01f32.json';
+          this.settingsPath = 'src/settings.20ce4.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- index.e1e5b.js
+++ index.e1e5b.js
@@ -1,4 +1,4 @@
-System.register(["./application.0747b.js"], function (_export, _context) {
+System.register(["./application.734e4.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
