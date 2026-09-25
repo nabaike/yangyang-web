800aa23b8a6332a95fe543f0c0ec82f6e5fe9f61
nabaike <nabaike@gmail.com>
2026-09-25 22:39:57 +0800

    update web 20260925-223957

--- index.34486.js
+++ index.34486.js
@@ -1,4 +1,4 @@
-System.register(["./application.8d17e.js"], function (_export, _context) {
+System.register(["./application.6498f.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
